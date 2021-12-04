64374921e71652637cd8662675ee8440493c369a
vishnunadh <vishnunadh222@gmail.com>
2021-12-04 23:22:51 +0530

    starting project

---
+++ plotTruss.py
@@ -0,0 +1,7 @@
+import matplotlib.pytplot as plt
+
+class Plot_Truss:
+    """Class plot the truss with the member thickness propotional to the damage condition and to visualise the results"""
+    
+    def __init__():
+        pass
